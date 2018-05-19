ad95b6c5dc1ca2d364f20735a1a2446177b97a5e
iemaosman <32035575+iemaosman@users.noreply.github.com>
2018-05-19 15:27:39 +0800

    Add files via upload

---
+++ SCM_Project/SCM_Project/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCM_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("==================================");
+            Console.WriteLine("SCM PROJECT");
+            Console.WriteLine("==================================");
+            Console.WriteLine("This a test for SCM Project");
+            Console.WriteLine("==================================");
+            Console.ReadLine();
+        }
+    }
+}
